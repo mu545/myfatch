e84f2a261375b3d4639110f5c0afdaeed9a562dd
Musa Sutisna <mus4.sutisn4@gmail.com>
2020-03-07 13:25:25 +0700

    Initial feature

---
+++ README.md
@@ -0,0 +1,102 @@
+# petch
+Alternative javascript fetch api with adding pre-setup options for every request.
+
+## Installation
+This is library available through [npm registry](https://npmjs.com/petch).
+
+```
+npm install --save petch
+```
+
+## Setup
+Lets setup your options and headers for fetch request.
+
+```js
+import * as Petch from 'petch'
+
+const petch = new Petch({
+  origin: 'https://example.com/api',
+  options: {
+    mode: 'cors'
+  },
+  headers: {
+    'Accept-Type': 'application/json'
+  }
+})
+```
+
+The `Petch()` accept one setting parameter with optionaly settings.
+
+| Name | Type | Description |
+|--|--|--|
+| origin | {String} | Where a fetch request will be send, the origin is prefix for all send request. |
+| options | {Object} | The root options of fetch api like **mode**, **cache**, etc.|
+| headers | {Object} | Headers options. |
+
+## Send request method
+### get()
+Send request with method **GET**.
+
+```js
+petch.get('users')
+  .then(res => {
+    console.log(res.ok)
+  })
+```
+
+### postData()
+Send request with method **POST**. Automaticaly add headers **Content-Type** with value `application/x-www-form-urlencoded` and transform body object into form data.
+
+```js
+petch.postData('login', {
+  body: {
+    username: 'username',
+    password: 'password'
+  }
+})
+  .then(res => {
+    console.log(res.ok)
+  })
+```
+
+### uploadData()
+Send request with method **POST**. Automaticaly add headers **Content-Type** with value `multipart/form-data` and transform body object into form data.
+
+```js
+const fileField = document.querySelector('input[type="file"]');
+
+petch.uploadData('user/profile', {
+  body: {
+    photo: fileField.files[0]
+  }
+})
+  .then(res => {
+    console.log(res.ok)
+  })
+```
+
+### postJSON()
+Send request with method **POST**. Automaticaly add headers **Content-Type** with value `application/json` and transform body object into json data.
+
+```js
+petch.postJSON('user/profile', {
+  body: {
+    first_name: 'FirstName',
+    last_name: 'LastName'
+  }
+})
+  .then(res => {
+    console.log(res.ok)
+  })
+```
+
+## Parameters
+All request method can accept two parameter and return a fetch promise.
+
+| Name | Type | Description |
+|--|--|--|
+| url | {String} | The path of request url. |
+| setting | {Object} | Contain fetch setting. |
+
+## PUT & DELETE
+Same with **GET** and **POST** you can use **PUT** and **DELETE** method with similliar parameter.
---
+++ index.js
@@ -0,0 +1,150 @@
+function Petch ({origin = null, options = {}, headers = {}} = {}) {
+  // a.k.a this
+  const self = this
+
+  const setting = {
+    origin: window.location.origin,
+    options: {},
+    headers: {}
+  }
+
+  /**
+   * Apply all received setting parameters.
+   *
+   * @return  void
+   */
+  const setup = function () {
+    setting.origin = origin || setting.origin
+    setting.options = {...setting.options, ...options}
+    setting.headers = {...setting.headers, ...headers}
+  }
+
+  /**
+   * Create options from setting.
+   *
+   * @param   object
+   * @param   object
+   * @param   object
+   * @return  object
+   */
+  const getOptions = function (options, headers, body) {
+    let currentOptions = {...setting.options, ...options}
+
+    currentOptions.headers = {...setting.headers, ...headers}
+   
+    if (typeof body !== null) {
+      currentOptions.body = body
+    }
+
+    return currentOptions
+  }
+
+  /**
+   * Create form data.
+   *
+   * @param   object
+   * @return  object
+   */
+  const createFormData = function (body) {
+    let formData = new FormData()
+
+    for (var field in body) {
+      formData.append(field, body[field])
+    }
+
+    return formData
+  }
+
+  /**
+   * Request with get method.
+   *
+   * @param   string
+   * @param   object
+   * @return  promise
+   */
+  this.get = function (url, {options = {}, headers = {}} = {}) {
+    return fetch(`${setting.origin}/${url}`, getOptions(options, headers, null))
+  }
+
+  /**
+   * Request with post method.
+   *
+   * @param   string
+   * @param   object
+   * @return  promise
+   */
+  this.postData = function (url, {options = {}, headers = {}, body = {}} = {}) {
+    options['method'] = 'POST'
+
+    headers['Content-Type'] = 'application/x-www-form-urlencoded'
+
+    return fetch(`${setting.origin}/${url}`, getOptions(options, headers, createFormData(body)))
+  }
+
+  /**
+   * Request with delete method.
+   *
+   * @param   string
+   * @param   object
+   * @return  promise
+   */
+  this.deleteData = function (url, {options = {}, headers = {}, body = {}} = {}) {
+    options['method'] = 'DELETE'
+
+    headers['Content-Type'] = 'application/x-www-form-urlencoded'
+
+    return fetch(`${setting.origin}/${url}`, getOptions(options, headers, createFormData(body)))
+  }
+
+
+  /**
+   * Request with post method to upload files.
+   *
+   * @param   string
+   * @param   object
+   * @return  promise
+   */
+  this.uploadData = function (url, {options = {}, headers = {}, body = {}} = {}) {
+    options['method'] = 'POST'
+
+    headers['Content-Type'] = 'multipart/form-data'
+
+    return fetch(`${setting.origin}/${url}`, getOptions(options, headers, createFormData(body)))
+  }
+
+  /**
+   * Request with post data to send json body.
+   *
+   * @param   string
+   * @param   object
+   * @return  promise
+   */
+  this.postJSON = function (url, {options = {}, headers = {}, body = {}} = {}) {
+    options['method'] = 'POST'
+
+    headers['Content-Type'] = 'application/json'
+
+    return fetch(`${setting.origin}/${url}`, getOptions(options, headers, JSON.stringify(body)))
+  }
+
+  /**
+   * Request with delete method to send json body.
+   *
+   * @param   string
+   * @param   object
+   * @return  promise
+   */
+  this.deleteJSON = function (url, {options = {}, headers = {}, body = {}} = {}) {
+    options['method'] = 'DELETE'
+
+    headers['Content-Type'] = 'application/json'
+
+    return fetch(`${setting.origin}/${url}`, getOptions(options, headers, JSON.stringify(body)))
+  }
+
+  // Pre-setup initialization
+  setup()
+}
+
+
+module.exports = Petch
